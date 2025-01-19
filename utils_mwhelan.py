45f7b12298b4d4d7b77021d080ecc03d04d5d6a6
meghanwithanh <115429532+meghanwithanh@users.noreply.github.com>
2025-01-19 16:18:10 -0600

    Update utils_mwhelan.py

--- utils_mwhelan.py
+++ utils_mwhelan.py
@@ -1,6 +1,6 @@
 
 """
-Module: utils_case
+Module: utils_mwhelan
 
 Purpose: Reusable Module for My Analytics Projects
 
@@ -8,7 +8,7 @@ Description: This module provides a byline for my analytics projects.
 When we work hard to write useful code, we want it to be reusable.
 A good byline could be used in every Python analytics project we do.
 
-Author: Denise Case
+Author: Meghan Whelan
 
 TODO: Change the module name in this opening docstring
 TODO: Change the author in this opening docstring
